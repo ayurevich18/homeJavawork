f0083f395340b3deb35a92a74fd9bbc6fab4fd02
ayurevich18 <38555940+ayurevich18@users.noreply.github.com>
2019-04-13 10:59:52 +0300

    Update Task2.java

--- src/homejava/Task2.java
+++ src/homejava/Task2.java
@@ -8,10 +8,10 @@ public class Task2 {
         int d=9;
         int e=0;
 
-     if((a>=b)&&(a>=b)&&(a>=c))e++;
-     if((b>=a)&&(b>=c)&&(b>=d))e++;
-     if((c>=a)&&(c>=b)&&(c>=d))e++;
-     if((d>=a)&&(d>=b)&&(d>=c))e++;
+     if((a>=b)&(a>=b)&(a>=c))e++;
+     if((b>=a)&(b>=c)&(b>=d))e++;
+     if((c>=a)&(c>=b)&(c>=d))e++;
+     if((d>=a)&(d>=b)&(d>=c))e++;
      System.out.println("Количество одиннаковых максимальных чисел: "+e);
     }
 }
