445b04442dc5fc2e76d698327d9d9c9fe769be44
ayurevich18 <38555940+ayurevich18@users.noreply.github.com>
2019-04-13 11:02:54 +0300

    Update Task3.java

--- src/homejava/Task3.java
+++ src/homejava/Task3.java
@@ -10,19 +10,19 @@ public class Task3 {
         int max=0;
         int min=0;
 
-        if((a>b)&&(a>c)&&(a>d)&&(a>g))max=a;
-        if((b>a)&&(b>c)&&(b>d)&&(b>g))max=b;
-        if((c>a)&&(c>b)&&(c>d)&&(c>g))max=c;
-        if((d>a)&&(d>c)&&(d>b)&&(d>g))max=d;
-        if((g>a)&&(g>c)&&(g>d)&&(g>b))max=g;
+        if((a>b)&(a>c)&(a>d)&(a>g))max=a;
+        if((b>a)&(b>c)&(b>d)&(b>g))max=b;
+        if((c>a)&(c>b)&(c>d)&(c>g))max=c;
+        if((d>a)&(d>c)&(d>b)&(d>g))max=d;
+        if((g>a)&(g>c)&(g>d)&(g>b))max=g;
 
         System.out.println(max);
 
-        if((a<b)&&(a<c)&&(a<d)&&(a<g))min=a;
-        if((b<a)&&(b<c)&&(b<d)&&(b<g))min=b;
-        if((c<a)&&(c<b)&&(c<d)&&(c<g))min=c;
-        if((d<a)&&(d<c)&&(d<b)&&(d<g))min=d;
-        if((g<a)&&(g<c)&&(g<d)&&(g<b))min=g;
+        if((a<b)&(a<c)&(a<d)&(a<g))min=a;
+        if((b<a)&(b<c)&(b<d)&(b<g))min=b;
+        if((c<a)&(c<b)&(c<d)&(c<g))min=c;
+        if((d<a)&(d<c)&(d<b)&(d<g))min=d;
+        if((g<a)&(g<c)&(g<d)&(g<b))min=g;
 
         System.out.println(min);
 
